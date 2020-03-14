974f1652c5a98c3413d2d601163139d4b5acd221
Eduardo Gam <60325791+EduGame3@users.noreply.github.com>
2020-03-14 08:51:42 -0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,9 +2,9 @@ print('COMPARADOR DE NÚMEROS')
 a = float(input('Escriba un número: ')) #¿cómo hacer para que acepte el 1/2?
 b = float(input('Escriba otro número: '))
 if (a < b):
-	print(f"Menor: {a} ; Mayor: {b}")
+	print("hola")
 elif (a > b):
-	print(f"Menor: {b} ; Mayor: {a}")
+	print("adios")
 else:
 	print('Los dos números son iguales')
 
